d1b0a9e17383b928cc4e5a3897eb91f953a1adef
zmcg3000 <zacamcgill@protonmail.com>
2023-12-11 15:03:29 +1100

    Made some changes to fix the issue with landscape orientation view

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Image, Pressable, Dimensions } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,7 +12,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import * as Font from 'expo-font';
 import AccessibilitySettings from './accessibilitySettings';
 import { SettingsProvider } from './settingsContext';
-import Orientation from 'react-native-orientation-locker'; // Import Orientation
+import * as ScreenOrientation from 'expo-screen-orientation';
 
 const Stack = createNativeStackNavigator();
 
@@ -43,37 +43,37 @@ export default function App() {
 
   // Determine initial orientation and style
   const deviceWidth = Dimensions.get('window').width;
-  const deviceHeight = Dimensions.get('window').height;
   const isTablet = deviceWidth > 768; // Adjust this value for tablet size if needed
   const initialStyles = isTablet ? tabletStyles : mobileStyles;
   const [currentStyles, setCurrentStyles] = useState(initialStyles);
 
   const openSettings = () => setSettingsVisible(true);
 
-  const handleOrientationChange = useCallback(() => {
-    const newDeviceWidth = Dimensions.get('window').width;
-    const newDeviceHeight = Dimensions.get('window').height;
-    const newIsTablet = newDeviceWidth > 768; // Re-check tablet size if needed
-
-    setCurrentStyles(newIsTablet ? tabletStyles : mobileStyles);
-  }, []);
-
   useEffect(() => {
-    // Lock to landscape if tablet
-    if (isTablet) {
-      Orientation.lockToLandscape();
+    async function changeScreenOrientation() {
+      if (isTablet) {
+        await ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE_RIGHT);
+      }
     }
 
+    changeScreenOrientation();
+
     // Set initial styles and listen for orientation changes
-    handleOrientationChange();
-    Dimensions.addEventListener('change', handleOrientationChange);
+    const handleOrientationChange = () => {
+      const newDeviceWidth = Dimensions.get('window').width;
+      const newIsTablet = newDeviceWidth > 768;
+      setCurrentStyles(newIsTablet ? tabletStyles : mobileStyles);
+    };
+
+    // Subscribe to dimension changes
+    const subscription = Dimensions.addEventListener('change', handleOrientationChange);
 
     // Cleanup
     return () => {
-      Dimensions.removeEventListener('change', handleOrientationChange);
-      Orientation.unlockAllOrientations(); // Unlock orientation on unmount
+      subscription.remove();
+      ScreenOrientation.unlockAsync(); // Unlock orientation on unmount
     };
-  }, [handleOrientationChange, isTablet]);
+  }, [isTablet]);
 
   useEffect(() => {
     async function loadFonts() {
